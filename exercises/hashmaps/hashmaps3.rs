9f0070283698eb9ca1b3e41428cc04e8d323d825
Nostalgia <lv15623448685@gmail.com>
2025-03-22 11:08:31 +0000

    feat: finish until 51

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -30,15 +29,31 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
 
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
-        let team_1_name = v[0].to_string();
-        let team_1_score: u8 = v[2].parse().unwrap();
-        let team_2_name = v[1].to_string();
-        let team_2_score: u8 = v[3].parse().unwrap();
+        let team1_name = v[0].to_string();
+        let team1_score: u8 = v[2].parse().unwrap();
+        let team2_name = v[1].to_string();
+        let team2_score: u8 = v[3].parse().unwrap();
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+scores.entry(team1_name.clone()).and_modify(|team| {
+    team.goals_scored += team1_score;
+    team.goals_conceded += team2_score;
+}).or_insert(Team {
+    goals_scored: team1_score,
+    goals_conceded: team2_score,
+});
+
+// Update or insert team 2
+scores.entry(team2_name.clone()).and_modify(|team| {
+    team.goals_scored += team2_score;
+    team.goals_conceded += team1_score;
+}).or_insert(Team {
+    goals_scored: team2_score,
+    goals_conceded: team1_score,
+});
     }
     scores
 }
